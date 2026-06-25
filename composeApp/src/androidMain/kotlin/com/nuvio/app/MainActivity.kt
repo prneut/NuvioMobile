140c96242df024341f1bfd214fd035ce8051c516
prneut <prneut@hotmail.com>
2026-06-25 02:32:44 +0200

    fix: missing coroutine imports for notification prompt

--- composeApp/src/androidMain/kotlin/com/nuvio/app/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/nuvio/app/MainActivity.kt
@@ -3,9 +3,10 @@ package com.nuvio.app
 import android.content.Intent
 import android.content.res.Configuration
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.launch
 import androidx.activity.SystemBarStyle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
