491ae63e230d0ec5ccc6ecc21738b40acb9b1003
prneut <prneut@hotmail.com>
2026-06-25 02:14:37 +0200

    chore: rename app and id for testing side-by-side

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -51,7 +51,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.nuvio.app"
+        applicationId = "com.nuvio.app.optimal"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
         versionCode = releaseAppVersionCode
