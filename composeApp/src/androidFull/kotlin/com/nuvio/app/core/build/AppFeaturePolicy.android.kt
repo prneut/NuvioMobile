bf6d6bcc16af810c7857425d89350c58fcdba3e4
prneut <prneut@hotmail.com>
2026-07-08 13:26:09 +0200

    Disable in-app updater for androidFull to prevent overwriting custom fork

--- composeApp/src/androidFull/kotlin/com/nuvio/app/core/build/AppFeaturePolicy.android.kt
+++ composeApp/src/androidFull/kotlin/com/nuvio/app/core/build/AppFeaturePolicy.android.kt
@@ -8,6 +8,6 @@ actual object AppFeaturePolicy {
     actual val p2pEnabled: Boolean = true
     actual val trailerPlaybackMode: TrailerPlaybackMode = TrailerPlaybackMode.IN_APP
     actual val heroTrailerPlaybackSupported: Boolean = true
-    actual val inAppUpdaterEnabled: Boolean = true
+    actual val inAppUpdaterEnabled: Boolean = false
     actual val imdbRatingLogoEnabled: Boolean = true
 }
